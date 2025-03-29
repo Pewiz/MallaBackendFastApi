7de26cb9f70874756c8875f01a85759b6addd8f6
David Osorio <91290489+Pewiz@users.noreply.github.com>
2025-03-28 23:58:41 -0300

    Fix: problem with name the table

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import relationship
 from database import Base
 
 class Carrera (Base):
-    __tablenam__ = "carreras"
+    __tablename__ = "carreras"
 
     id = Column(Integer, primary_key=True, index=True)
     nombre = Column(String, unique=True, index=True)
@@ -17,7 +17,7 @@ class Ramo (Base):
     id = Column(Integer, primary_key=True, index=True)
     nombre = Column(String, index=True)
     semestre = Column(Integer)
-    carrera_id = Column(Integer, ForeignKey("carrera.id"))
+    carrera_id = Column(Integer, ForeignKey("carreras.id"))
 
     carrera = relationship("Carrera", back_populates="ramos")
     prerequisitos = relationship("Prerequisito", foreign_keys="[Prerequisito.ramo_id]", back_populates="ramo")
