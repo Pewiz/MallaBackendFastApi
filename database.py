df369161cc8c49950f54218ca4cca68017500d75
David Osorio <91290489+Pewiz@users.noreply.github.com>
2025-04-01 23:40:29 -0300

    Restore env database

--- database.py
+++ database.py
@@ -6,9 +6,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-POSTGRES_URL = os.getenv("POSTGRES_URL")
+URL_DATABASE = os.getenv("URL_DATABASE")
 
-engine = create_engine(POSTGRES_URL)
+engine = create_engine(URL_DATABASE)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
